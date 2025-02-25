4df26fcafa37e9672aec0c7deaf80f956aa14a95
poojaraju23 <poojaraju23@gmail.com>
2025-02-25 11:23:48 -0500

    The AND, OR, NOT Operators...1. AND operator

--- RetrievingDataFromaSingleTable.sql
+++ RetrievingDataFromaSingleTable.sql
@@ -1,8 +1,11 @@
--- Excercise : WHERE Clause Ques: Get the orders placed this year
+-- The AND, OR and NOT Operators : How to combine multiple search conditions when filtering data
+-- AND operator
 
 SELECT *
-FROM orders
-WHERE order_date >= "2019-01-01"
+FROM customers
+WHERE birth_date > "1990-01-01" AND points > 1000
+
+
 
 
 
