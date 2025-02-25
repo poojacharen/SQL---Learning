db5839c68b6649c1f109d8470baf9c7a5f062276
poojaraju23 <poojaraju23@gmail.com>
2025-02-25 14:55:39 -0500

    Exercise using BETWEEN Operator

--- RetrievingDataFromaSingleTable.sql
+++ RetrievingDataFromaSingleTable.sql
@@ -1,8 +1,8 @@
--- BETWEEN Operator - whenever we are comparing an attribute with a range of values 
+-- Exercise : Return customers born between 1/1/1990 and 1/1/2000
 
 SELECT *
 FROM customers
-WHERE points BETWEEN 1000 AND 3000
+WHERE birth_date BETWEEN "1990/01/01" AND "2000/01/01"
 
 
 
