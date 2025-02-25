87af6ed3eb3833c71e4b896a322bbb9e2c858026
poojaraju23 <poojaraju23@gmail.com>
2025-02-25 16:00:18 -0500

    Using IS NULL Operator

--- RetrievingDataFromaSingleTable.sql
+++ RetrievingDataFromaSingleTable.sql
@@ -1,13 +1,11 @@
--- REGEXP : Regular Expression - extremely powerful when searching for a string. They allow us to search for most complex patterns. We can use ^ a beginning 
--- of a string. $ to represent a end of a string. | represents to search for multiple search patterns of the string eg: "field|mac|rose"
--- "[gim]e" - represents any charecters that are present before e and it can also be "e[gim]", and also we can give some range of char like "[a-h]e"
--- ^ : beginning of a string, $ : end of a string, | : logical or(multipe searh patterns), [abcdef] : any single char listed in the brackets, [-] : to represent a range like [a-h].
-
--- Exercise : 4. Get the customers whose last names contain B followed by R or U
+-- NULL Operator : is a absence of a value
+-- we use IS NULL where it shows null value of the customer, IS NOT NULL where it shows all values without null
 
 SELECT *
 FROM customers
-WHERE last_name REGEXP "b[ru]"
+WHERE phone IS NULL
+
+
 
 
 
