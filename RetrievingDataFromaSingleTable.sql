d1ee0051a0eb374ee6fab070552a300887c4550a
poojaraju23 <poojaraju23@gmail.com>
2025-02-25 15:07:23 -0500

    Using LIKE Operator

--- RetrievingDataFromaSingleTable.sql
+++ RetrievingDataFromaSingleTable.sql
@@ -1,8 +1,15 @@
--- Exercise : Return customers born between 1/1/1990 and 1/1/2000
+-- LIKE Operator -- % this means it can have any number of characters, %b% this meand it can have any num of characters in front of b, in the back or in the middle it doesn't matter.
+-- eg: "_y" : this matches a single character (this means it has only 2 characters long), "_____y" : this matches with 6 characters
+-- In simple : % any number of characters
+--             _ single character
 
 SELECT *
 FROM customers
-WHERE birth_date BETWEEN "1990/01/01" AND "2000/01/01"
+WHERE last_name LIKE "_____y"
+
+
+
+
 
 
 
