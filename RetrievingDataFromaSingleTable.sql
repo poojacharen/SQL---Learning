3ce7892e8bd8c5c8fa53dbb750d5e7fe3e085daf
poojaraju23 <poojaraju23@gmail.com>
2025-02-25 11:28:07 -0500

    Used OR, AND operators

--- RetrievingDataFromaSingleTable.sql
+++ RetrievingDataFromaSingleTable.sql
@@ -1,9 +1,10 @@
 -- The AND, OR and NOT Operators : How to combine multiple search conditions when filtering data
--- OR Operator
+-- OR, AND Operator
 
 SELECT *
 FROM customers
-WHERE birth_date > "1990-01-01" OR points > 1000
+WHERE birth_date > "1990-01-01" OR points > 1000 AND
+      state = "VA"
 
 
 
