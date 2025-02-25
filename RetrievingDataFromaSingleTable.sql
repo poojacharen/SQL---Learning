9bb22c8f7ab1fc01d6f8c8b18e310a2811d54d2e
poojaraju23 <poojaraju23@gmail.com>
2025-02-25 14:36:34 -0500

    Using IN Operator

--- RetrievingDataFromaSingleTable.sql
+++ RetrievingDataFromaSingleTable.sql
@@ -1,8 +1,10 @@
--- Exercise : From the order_items table, get the items for order #6 where the total price is greater than 30
+-- IN Operator
 
 SELECT *
-FROM order_items
-WHERE order_id = 6 AND quantity * unit_price > 30
+FROM customers
+WHERE state IN ("VA", "FL", "GA") 
+
+
 
   
 
