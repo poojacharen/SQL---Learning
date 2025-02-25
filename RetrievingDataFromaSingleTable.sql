c8f4fa6d55ffb4aa354892194c8e57492653e1e1
poojaraju23 <poojaraju23@gmail.com>
2025-02-25 11:33:49 -0500

    Used NOT, OR Operators

--- RetrievingDataFromaSingleTable.sql
+++ RetrievingDataFromaSingleTable.sql
@@ -1,9 +1,9 @@
 -- The AND, OR and NOT Operators : How to combine multiple search conditions when filtering data
--- NOT
+-- NOT, OR Operator
 
 SELECT *
 FROM customers
-WHERE NOT birth_date > "1990-01-01" 
+WHERE NOT (birth_date > "1990-01-01" OR points > 1000)
 
 
 
