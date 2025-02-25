1de9ee8b859bb3a881f546aa66015513231a01ca
poojaraju23 <poojaraju23@gmail.com>
2025-02-25 11:38:04 -0500

    How to simplify the NOT operator query without using NOT

--- RetrievingDataFromaSingleTable.sql
+++ RetrievingDataFromaSingleTable.sql
@@ -1,9 +1,9 @@
 -- The AND, OR and NOT Operators : How to combine multiple search conditions when filtering data
--- NOT, OR Operator
+-- We can simplify without using NOT operator
 
 SELECT *
 FROM customers
-WHERE NOT (birth_date > "1990-01-01" OR points > 1000)
+WHERE  birth_date <= "1990-01-01" AND points <= 1000
 
 
 
