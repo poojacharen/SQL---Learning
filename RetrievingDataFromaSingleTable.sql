2c57cef20301d0bc4b6a7258fbdce1f857a2b2d9
poojaraju23 <poojaraju23@gmail.com>
2025-02-25 15:52:25 -0500

    REGEXP exercise (3).

--- RetrievingDataFromaSingleTable.sql
+++ RetrievingDataFromaSingleTable.sql
@@ -3,11 +3,11 @@
 -- "[gim]e" - represents any charecters that are present before e and it can also be "e[gim]", and also we can give some range of char like "[a-h]e"
 -- ^ : beginning of a string, $ : end of a string, | : logical or(multipe searh patterns), [abcdef] : any single char listed in the brackets, [-] : to represent a range like [a-h].
 
--- Exercise : 2. Get the customers whose last names end with EY or ON
+-- Exercise : 3. Get the customers whose last names starts with MY or contains SE
 
 SELECT *
 FROM customers
-WHERE last_name REGEXP "ey$|on$"
+WHERE last_name REGEXP "^my|se"
 
 
 
