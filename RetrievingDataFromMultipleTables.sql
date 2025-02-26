7aa1b449b001df01f61f3bf1c2fe7a9594ba2bad
poojaraju23 <poojaraju23@gmail.com>
2025-02-26 15:12:52 -0500

    Cross Joins Exercise with both Implicit and Explicit Syntax Method

--- RetrievingDataFromMultipleTables.sql
+++ RetrievingDataFromMultipleTables.sql
@@ -1,13 +1,15 @@
 -- Cross Joins : It is used to combine or join every record of first table in every record of the second table
--- A real example of using Cross Join is where we have a table of sizes like small, medium, large and table of colours like red, blue, green
--- and then we want to combine all the sizes with all the colors ie., when we use a cross join
--- Now, the query which is written below is an eg. of how to use it.
--- This is an implicit query, if its explicit then the query looks like FROM customers c, products p (without mentioning cross join) it can be 
---                                                                                                        written like this too.)
+-- Exercise : Do a cross join b/w shippers and products, using the implicit and explicit syntax
 
+-- Explicit syntax metho
 SELECT 
-      c.first_name AS customer,
-      p.name AS product
-FROM customers c
+      p.name AS product,
+      sh.name AS shipper
+FROM shippers sh
 CROSS JOIN products p
-ORDER BY c.customer_id
+ORDER BY sh.name
+
+-- Implicit syntax method
+-- SELECT *
+-- FROM shippers sh, products p
+-- ORDER BY sh.name
