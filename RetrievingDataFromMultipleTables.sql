b314201d49810a457620141c8ff32a71b3adfadd
poojaraju23 <poojaraju23@gmail.com>
2025-02-26 11:27:47 -0500

    Implicit Join Syntax

--- RetrievingDataFromMultipleTables.sql
+++ RetrievingDataFromMultipleTables.sql
@@ -1,9 +1,14 @@
--- Compound Join Conditions : When you join two tables using more than one condition, combining multiple columns to match rows from both tables.
+-- Implicit Join Syntax : This method is not recommended
 
-USE sql_store;
 SELECT *
-FROM order_items oi
-JOIN order_item_notes oin
-    ON oi.order_id = oin.order_id
-    AND oi.product_id = oin.product_id
+FROM orders o
+JOIN customers c
+    ON o.customer_id = c.customer_id
+    
+-- Implicit Join Syntax
+-- SELECT *
+-- FROM orders o, customers c
+-- WHERE o.customer_id = c.customer_id   
+-- This method is not suggested because when we accidently forget the where clause then we will get a cross join. 
+-- Cross join means that when we forget where clause then every record in the orders and customer table will be cross joined. There will be 100 rec.
 
