4909a447e56be8ce3890a9f3151d6582b4faf1b6
poojaraju23 <poojaraju23@gmail.com>
2025-02-26 11:01:08 -0500

    Joining Multiple Tables Exercise

--- RetrievingDataFromMultipleTables.sql
+++ RetrievingDataFromMultipleTables.sql
@@ -1,17 +1,18 @@
 -- Joining Multiple tables
+-- Exercise : Look at the sql-invocing db, see the payments table. We can join the client_id to the clients table to see the name of the clients. 
+--            Next, in the same payments table we have invoice_id, date, amount & payment_method. So similarly we can join this table to the 
+--            payments_methods table. Write a query and joins this table with the payments_methods table as well as the clients table.
 
-USE sql_store;
+USE sql_invoicing;
 SELECT 
-     o.order_id,
-     o.order_date,
-     c.first_name,
-     c.last_name,
-     os.name AS status
-FROM orders o
-JOIN customers c
-    ON o.customer_id = c.customer_id
-JOIN order_statuses os
-    ON o.status = os.order_status_id    
-
-
+     p.date,
+     p.invoice_id,
+     p.amount,
+     c.name,
+     pm.name
+FROM payments p
+JOIN clients c
+    ON p.client_id = c.client_id
+JOIN payment_methods pm
+    ON p.payment_method = pm.payment_method_id  
 
