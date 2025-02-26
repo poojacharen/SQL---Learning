6d7e4632ea0013c70b72ca4d2e17200c8ca8edec
poojaraju23 <poojaraju23@gmail.com>
2025-02-26 11:18:20 -0500

    Compound Join Conditions

--- RetrievingDataFromMultipleTables.sql
+++ RetrievingDataFromMultipleTables.sql
@@ -1,18 +1,9 @@
--- Joining Multiple tables
--- Exercise : Look at the sql-invocing db, see the payments table. We can join the client_id to the clients table to see the name of the clients. 
---            Next, in the same payments table we have invoice_id, date, amount & payment_method. So similarly we can join this table to the 
---            payments_methods table. Write a query and joins this table with the payments_methods table as well as the clients table.
+-- Compound Join Conditions : When you join two tables using more than one condition, combining multiple columns to match rows from both tables.
 
-USE sql_invoicing;
-SELECT 
-     p.date,
-     p.invoice_id,
-     p.amount,
-     c.name,
-     pm.name
-FROM payments p
-JOIN clients c
-    ON p.client_id = c.client_id
-JOIN payment_methods pm
-    ON p.payment_method = pm.payment_method_id  
+USE sql_store;
+SELECT *
+FROM order_items oi
+JOIN order_item_notes oin
+    ON oi.order_id = oin.order_id
+    AND oi.product_id = oin.product_id
 
