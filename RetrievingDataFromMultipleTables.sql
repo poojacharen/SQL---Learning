979e8905f51a0a79fda53257e1a821c44dc64399
poojaraju23 <poojaraju23@gmail.com>
2025-02-26 10:31:05 -0500

    Joining Multiple Tables

--- RetrievingDataFromMultipleTables.sql
+++ RetrievingDataFromMultipleTables.sql
@@ -1,12 +1,17 @@
--- Self Joins : Joining a table with itself
+-- Joining Multiple tables
 
-USE sql_hr;
+USE sql_store;
 SELECT 
-      e.employee_id, 
-      e.first_name, 
-      m.first_name manager
-FROM employees e
-JOIN employees m
-     ON e.reports_to = m.employee_id
+     o.order_id,
+     o.order_date,
+     c.first_name,
+     c.last_name,
+     os.name AS status
+FROM orders o
+JOIN customers c
+    ON o.customer_id = c.customer_id
+JOIN order_statuses os
+    ON o.status = os.order_status_id    
+
 
 
