125adc41f6b9e1c04da04ce543ede049622150b6
poojaraju23 <poojaraju23@gmail.com>
2025-02-26 14:45:00 -0500

    The USING Clause Exercise

--- RetrievingDataFromMultipleTables.sql
+++ RetrievingDataFromMultipleTables.sql
@@ -1,7 +1,15 @@
--- The USING Clause Method 2
+-- The USING Clause 
+-- Exercise :From the sql_invoicing db, write the query to select the payments on the payments table and give the result with date, client,
+--           amount, payment method (name).
 
-USE sql_store;
-SELECT *
-FROM order_items oi
-JOIN order_item_notes oin
-    USING (order_id, product_id)
+USE sql_invoicing;
+SELECT 
+      p.date,
+      c.name AS client,
+      p.amount,
+      pm.name AS paymenttype
+FROM payments p
+JOIN clients c
+    USING (client_id)
+JOIN payment_methods pm
+    ON p.payment_method = pm.payment_method_id  
