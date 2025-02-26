d62d895ce20ea1488cfc09c46beb31c328c32325
poojaraju23 <poojaraju23@gmail.com>
2025-02-26 14:07:02 -0500

    Outer JOin B/W Multiple Tables Exercise

--- RetrievingDataFromMultipleTables.sql
+++ RetrievingDataFromMultipleTables.sql
@@ -1,13 +1,17 @@
 -- Outer Join Between Multiple Tables
+-- Exercise : Write a query which produces this result order_date, order_id, first_name, shipper, status.
 
 SELECT 
-      c.customer_id,
-      c.first_name,
+      o.order_date,
       o.order_id,
-      sh.name AS shipper
-FROM customers c
-LEFT JOIN orders o
-	ON c.customer_id = o.customer_id
+      c.first_name,
+      sh.name AS shipper,
+      os.name AS status
+FROM orders o
+LEFT JOIN customers c
+    ON o.customer_id = c.customer_id
 LEFT JOIN shippers sh
-    ON   o.shipper_id = sh.shipper_id  
-ORDER BY c.customer_id    
+    ON o.shipper_id = sh.shipper_id
+LEFT JOIN order_statuses os
+    ON o.status = os.order_status_id  
+   
