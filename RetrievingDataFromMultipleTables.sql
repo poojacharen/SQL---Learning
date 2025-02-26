476499f14c5b725a7a7993d9b3ffdda3952278f3
poojaraju23 <poojaraju23@gmail.com>
2025-02-26 15:46:06 -0500

    Union Exercise

--- RetrievingDataFromMultipleTables.sql
+++ RetrievingDataFromMultipleTables.sql
@@ -1,8 +1,31 @@
 -- Unions : We can combine records from multiple queries
--- Method 2 : We combining queries with multiple table
+-- Exercise : Write a query to produce the report with customer_id, first_name, points, type
+--            less than 2000 points : type - Bronze,  2000 to 3000 points : type - silver,  more than 3000 points : type - gold  
 
-SELECT first_name
-FROM customers
+SELECT 
+      customer_id,
+      first_name,
+      points,
+      "Bronze" AS type
+FROM customers 
+WHERE points < 2000
 UNION
-SELECT name
-FROM shippers
+SELECT 
+      customer_id,
+      first_name,
+      points,
+      "Silver" AS type
+FROM customers 
+WHERE points BETWEEN 2000 AND 3000 
+UNION
+SELECT 
+      customer_id,
+      first_name,
+      points,
+      "Gold" AS type
+FROM customers 
+WHERE points > 3000
+ORDER BY first_name
+
+
+        
