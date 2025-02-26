b8d989eebd902e6e909f39473188eb940bfde452
poojaraju23 <poojaraju23@gmail.com>
2025-02-26 15:26:11 -0500

    Unions Method 2

--- RetrievingDataFromMultipleTables.sql
+++ RetrievingDataFromMultipleTables.sql
@@ -1,16 +1,8 @@
 -- Unions : We can combine records from multiple queries
--- Method 1 : We combined queries with same table
+-- Method 2 : We combining queries with multiple table
 
-SELECT 
-      order_id,
-      order_date,
-      "Active" AS status
-FROM orders
-WHERE order_date >= "2019-01-01" 
+SELECT first_name
+FROM customers
 UNION
-SELECT 
-      order_id,
-      order_date,
-      "Archived" AS status
-FROM orders
-WHERE order_date < "2019-01-01"
+SELECT name
+FROM shippers
