59a92afcb5f1c5534a9a059df39f246e8172ee3a
poojaraju23 <poojaraju23@gmail.com>
2025-02-26 13:26:15 -0500

    Outer Joins Exercise

--- RetrievingDataFromMultipleTables.sql
+++ RetrievingDataFromMultipleTables.sql
@@ -1,10 +1,11 @@
 -- Outer Joins : there are two joins RIGHT Join and LEFT join
+-- Exercise : Join the products table with order items table. And see how many times the product has been ordered. Output: product_id, name, qty
 
 SELECT 
-      c.customer_id,
-      c.first_name,
-      o.order_id
-FROM orders o
-RIGHT JOIN customers c
-    ON c.customer_id = o.customer_id
-ORDER BY c.customer_id    
+      p.product_id,
+      p.name,
+      oi.quantity
+FROM products p
+LEFT JOIN order_items oi
+	ON p.product_id = oi.product_id
+ORDER BY p.product_id    
