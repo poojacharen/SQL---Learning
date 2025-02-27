08e10fbf8c74dc54add3b3df9c21e3cf659c2030
poojaraju23 <poojaraju23@gmail.com>
2025-02-27 13:14:30 -0500

    Using Subqueries in Updates Exercise

--- InsertingUpdatingAndDeletingData.sql
+++ InsertingUpdatingAndDeletingData.sql
@@ -1,12 +1,13 @@
 -- Using Subqueries in Updates 
+-- Exercise : In the sql_store db look at the orders table as you can see some of the orders do not have a comment. write a sql statement to update
+--            the comments for orders for customers who have more than 3000 points. So customer with more than 3000 pts regard them as gold customer.
+--            Find their orders if they have placed an order. Update the comments column and set it to gold customer.  
 
-USE sql_invoicing;
-UPDATE invoices
-SET 
-   payment_total = invoice_total * 0.5,
-   payment_date = due_date
-WHERE client_id IN
-               (SELECT client_id
-                FROM clients
-                WHERE state IN ("CA", "NY"))
+USE sql_store;
+UPDATE orders
+SET comments = "Gold Customer"
+WHERE customer_id IN
+                  (SELECT customer_id
+				   FROM customers
+                   WHERE points > 3000 )
 
