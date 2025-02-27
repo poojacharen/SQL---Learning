2b839791f9bebfaa5942b0f40c24ba0219cc08ac
poojaraju23 <poojaraju23@gmail.com>
2025-02-27 12:30:10 -0500

    Updating a Single Row Method 2

--- InsertingUpdatingAndDeletingData.sql
+++ InsertingUpdatingAndDeletingData.sql
@@ -1,8 +1,22 @@
 -- Updating a Single Row
--- Method 1 : IN the invoices table, we see there is no payment and payment date in the first row with id 1. Lets imagine that the client has paid 
---            the amount but forget to upfate it, then the query should be like
+-- Method 2 : Let's imagine we have updated the wrong record and updated the record 1 instead of record 3. So we need to update the record one 
+--            more time and restore the values in the 1st column to 3rd, so the query be like,
+-- Now we have chnaged the 1st record to the orginal one 
+
+-- USE sql_invoicing;
+-- UPDATE invoices
+-- SET payment_total = DEFAULT, payment_date = NULL
+-- WHERE invoice_id = 1
+-- Now updating the 3rd record, lets imagine the cient made 50% of the total amount on the due date. Query be like,
 
 USE sql_invoicing;
 UPDATE invoices
-SET payment_total = 10, payment_date = "2019-02-06"
-WHERE invoice_id = 1
+SET 
+    payment_total = invoice_total * 0.5,
+    payment_date = due_date
+WHERE invoice_id = 3
+
+
+
+
+
