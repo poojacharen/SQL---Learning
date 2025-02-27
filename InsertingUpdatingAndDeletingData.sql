bab12f2e2e206025745601425f915177639c40d7
poojaraju23 <poojaraju23@gmail.com>
2025-02-27 12:19:29 -0500

    Updating a Single Row Method 1

--- InsertingUpdatingAndDeletingData.sql
+++ InsertingUpdatingAndDeletingData.sql
@@ -1,22 +1,8 @@
--- Creating a Copy of a Table : Copying data from one table to another
--- Exercise : Look at the invoices table, we have the columns. So create a copy of the records in this table and create a new table called
---            invoices_archive. However in that table instead of the client_id column we are gonna have client_name column so you need to join
---            this table with clients table and then use this query as sub query and create table statement. Also, you need to copy only the 
---            invoices that do have the payment
+-- Updating a Single Row
+-- Method 1 : IN the invoices table, we see there is no payment and payment date in the first row with id 1. Lets imagine that the client has paid 
+--            the amount but forget to upfate it, then the query should be like
 
 USE sql_invoicing;
-
-CREATE TABLE invoices_archived AS
-SELECT 
-      i.invoice_id,
-      i.number,
-      c.name AS client,
-      i.invoice_total,
-      i.payment_total,
-      i.invoice_date,
-      i.payment_date,
-      i.due_date
-FROM invoices i
-JOIN clients c
-    USING (client_id)
-WHERE payment_date IS NOT NULL
+UPDATE invoices
+SET payment_total = 10, payment_date = "2019-02-06"
+WHERE invoice_id = 1
