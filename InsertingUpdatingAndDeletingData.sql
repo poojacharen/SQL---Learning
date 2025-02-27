3c348fa4cba5f3e7dae4fec34b8af8ab78a97d6d
poojaraju23 <poojaraju23@gmail.com>
2025-02-27 10:48:51 -0500

    Inserting a Single Row

---
+++ InsertingUpdatingAndDeletingData.sql
@@ -0,0 +1,22 @@
+-- Inserting a Single Row
+
+
+INSERT INTO customers (
+			customer_id,
+			first_name,
+            last_name,
+            birth_date,
+            phone,
+            address,
+            city,
+            state,
+            points)
+VALUES (DEFAULT,
+       "Charen",
+       "Koneti",
+       "1992-09-14",
+       Null,
+       "383 Albert St",
+       "Waterloo",
+       "CA",
+		0)
