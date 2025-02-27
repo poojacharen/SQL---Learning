7f18425b2452cc75a38d495787632505ff22a79c
poojaraju23 <poojaraju23@gmail.com>
2025-02-27 10:56:52 -0500

    Inserting Multiple Rows

--- InsertingUpdatingAndDeletingData.sql
+++ InsertingUpdatingAndDeletingData.sql
@@ -1,22 +1,8 @@
--- Inserting a Single Row
+-- Inserting Multiple Rows : We used shippers table for the query
+-- Note: for addimg multiple rows we need to add commas after each brackets
 
-
-INSERT INTO customers (
-			customer_id,
-			first_name,
-            last_name,
-            birth_date,
-            phone,
-            address,
-            city,
-            state,
-            points)
-VALUES (DEFAULT,
-       "Charen",
-       "Koneti",
-       "1992-09-14",
-       Null,
-       "383 Albert St",
-       "Waterloo",
-       "CA",
-		0)
+INSERT INTO shippers (name)
+VALUES ("Shipper1"),
+       ("Shipper2"),
+       ("SHipper3")
+    
