ffc0d3d1dee84cf9bfdac7bb6b5a5884e10587a7
poojaraju23 <poojaraju23@gmail.com>
2025-02-27 11:10:22 -0500

    Inserting Multiple Rows Exercise

--- InsertingUpdatingAndDeletingData.sql
+++ InsertingUpdatingAndDeletingData.sql
@@ -1,8 +1,11 @@
 -- Inserting Multiple Rows : We used shippers table for the query
 -- Note: for addimg multiple rows we need to add commas after each brackets
+-- Exercise : Insert three rows in the products table
 
-INSERT INTO shippers (name)
-VALUES ("Shipper1"),
-       ("Shipper2"),
-       ("SHipper3")
-    
+INSERT INTO products (name,
+                      quantity_in_stock,
+                      unit_price)
+VALUES ("Veg Stew", 7, 1.08),
+       ("Chicken Soup", 8, 1.09),
+       ("Biriyani", 9, 1.10)
+   
