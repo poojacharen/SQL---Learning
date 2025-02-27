737819c549d999ba86ab03ca4d33f3b901c7c194
poojaraju23 <poojaraju23@gmail.com>
2025-02-27 11:36:44 -0500

    Creating a Copy of a Table

--- InsertingUpdatingAndDeletingData.sql
+++ InsertingUpdatingAndDeletingData.sql
@@ -1,8 +1,6 @@
--- Inserting Hierarchical Rows : How to insert data in multiple tables
-   
-INSERT INTO orders (customer_id, order_date, status)
-VALUES (1, "2019-01-03", 1);
+-- Creating a Copy of a Table : Copying data from one table to another
 
-INSERT INTO order_items
-VALUES (LAST_INSERT_ID(), 1, 1, 2.95),
-       (LAST_INSERT_ID(), 2, 1, 4.95)
+INSERT INTO orders_archived
+SELECT *
+FROM orders
+WHERE order_date < "2019-01-01"
