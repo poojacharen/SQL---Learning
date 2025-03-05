921c5a42d15f46419c32f8f2402c017642038fdf
poojaraju23 <poojaraju23@gmail.com>
2025-03-05 12:09:14 -0500

    The HAVING Clause

--- SummarizingData.sql
+++ SummarizingData.sql
@@ -1,14 +1,12 @@
--- The GROUP BY Clause 
--- Exercise : You have three columns (date, payment_method, total_payments) we can see the total payments for each date and payment_method 
---            combination.  
+-- The HAVING Clause : Here we shouldn't use WHERE clause. 
+-- With WHERE Clause : We can filter the data before the rows are grouped
+-- With HAVING Clause : We can filter the data after the rows are grouped
 
 USE sql_invoicing;
 SELECT 
-      date,
-      pm.name AS payment_method,
-      SUM(amount) AS total_payments
-FROM payments p
-JOIN payment_methods pm
-    ON p.payment_method = pm.payment_method_id
-GROUP BY date, payment_method
-ORDER BY date
+     client_id,
+     SUM(invoice_total) AS total_sales,
+     COUNT(*) AS number_of_invoices
+FROM invoices
+GROUP BY client_id
+HAVING total_sales > 500 AND number_of_invoices > 5
