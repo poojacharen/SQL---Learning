3b11fda7d17e42bee6e5d969565c525b49ed7b58
poojaraju23 <poojaraju23@gmail.com>
2025-03-05 14:28:23 -0500

    The ROLLUP Operator Exercise

--- SummarizingData.sql
+++ SummarizingData.sql
@@ -1,11 +1,11 @@
 -- The ROLLUP Operator : it summarizes the data 
+-- Exercise : Write a query to produce a report with columns (payment_method, total) write the qiery against payments table
 
 USE sql_invoicing;
 SELECT 
-      state,
-      city,
-      SUM(invoice_total) AS total_sales
-FROM invoices i
-JOIN clients c
-    USING (client_id)
-GROUP BY state, city  WITH ROLLUP 
+      pm.name AS payment_method,
+      SUM(amount) AS total
+FROM payments p
+JOIN payment_methods pm
+    ON p.payment_method = pm.payment_method_id
+GROUP BY pm.name WITH ROLLUP
