9d97f5c5362a5eebf0d93c135af0f5aaf0464e7d
poojaraju23 <poojaraju23@gmail.com>
2025-03-05 12:24:19 -0500

    The HAVING Clause Exercise

--- SummarizingData.sql
+++ SummarizingData.sql
@@ -1,12 +1,21 @@
 -- The HAVING Clause : Here we shouldn't use WHERE clause. 
 -- With WHERE Clause : We can filter the data before the rows are grouped
 -- With HAVING Clause : We can filter the data after the rows are grouped
+-- Exercise : Get the customers located in Virginia who spent more than $100
 
-USE sql_invoicing;
+USE sql_store;
 SELECT 
-     client_id,
-     SUM(invoice_total) AS total_sales,
-     COUNT(*) AS number_of_invoices
-FROM invoices
-GROUP BY client_id
-HAVING total_sales > 500 AND number_of_invoices > 5
+      c.customer_id,
+      c.first_name,
+      c.last_name,
+      SUM(oi.quantity * oi.unit_price) AS total_sales
+FROM customers c
+JOIN orders o 
+    USING (customer_id)
+JOIN order_items oi 
+    USING (order_id)
+WHERE state = "VA"
+GROUP BY   c.customer_id,
+      c.first_name,
+      c.last_name
+HAVING total_sales > 100
