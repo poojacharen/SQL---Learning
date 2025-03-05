bf0d8b0a8d763b1ddbb49affb3670107de1fc5c1
poojaraju23 <poojaraju23@gmail.com>
2025-03-05 11:40:18 -0500

    The FROUP BY Clause Method 1

--- SummarizingData.sql
+++ SummarizingData.sql
@@ -1,31 +1,11 @@
--- Aggredate Functions - How to write queries that summarize data - MAX(), MIN(), AVG(), SUM(), COUNT()
--- Exercise : Write a query against invoices table to generate this report we have date_range, total_sales, total_payments, what_we_expect, 
---            You need to calculate the first half of the year, second half of the year, and total.
+-- The GROUP BY Clause 
+-- Method 1 : Grouping using single column
 
 USE sql_invoicing;
 SELECT 
-	  "First half of 2019" AS date_range,
-      SUM(invoice_total) AS total_sales,
-      SUM(payment_total) AS total_payments,
-      SUM(invoice_total - payment_total) AS what_we_expect
-FROM invoices    
-WHERE invoice_date 
-     BETWEEN "2019-01-01" AND "2019-06-30"  
-UNION
-SELECT 
-	  "Second half of 2019" AS date_range,
-      SUM(invoice_total) AS total_sales,
-      SUM(payment_total) AS total_payments,
-      SUM(invoice_total - payment_total) AS what_we_expect
-FROM invoices    
-WHERE invoice_date 
-     BETWEEN "2019-07-01" AND "2019-12-30"  
-UNION 
-SELECT 
-	  "Total" AS date_range,
-      SUM(invoice_total) AS total_sales,
-      SUM(payment_total) AS total_payments,
-      SUM(invoice_total - payment_total) AS what_we_expect
-FROM invoices    
-WHERE invoice_date 
-     BETWEEN "2019-01-01" AND "2019-12-30"  
+      client_id,
+     SUM(invoice_total) AS total_sales
+FROM invoices
+WHERE invoice_date >= "2019-07-01"
+GROUP BY client_id
+ORDER BY total_sales DESC
