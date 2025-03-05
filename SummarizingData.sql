347355ae8e86a8e63fa6bc9e5bbbb1bb15c15b68
poojaraju23 <poojaraju23@gmail.com>
2025-03-05 14:20:36 -0500

    The ROLLUP Operator

--- SummarizingData.sql
+++ SummarizingData.sql
@@ -1,21 +1,11 @@
--- The HAVING Clause : Here we shouldn't use WHERE clause. 
--- With WHERE Clause : We can filter the data before the rows are grouped
--- With HAVING Clause : We can filter the data after the rows are grouped
--- Exercise : Get the customers located in Virginia who spent more than $100
+-- The ROLLUP Operator : it summarizes the data 
 
-USE sql_store;
+USE sql_invoicing;
 SELECT 
-      c.customer_id,
-      c.first_name,
-      c.last_name,
-      SUM(oi.quantity * oi.unit_price) AS total_sales
-FROM customers c
-JOIN orders o 
-    USING (customer_id)
-JOIN order_items oi 
-    USING (order_id)
-WHERE state = "VA"
-GROUP BY   c.customer_id,
-      c.first_name,
-      c.last_name
-HAVING total_sales > 100
+      state,
+      city,
+      SUM(invoice_total) AS total_sales
+FROM invoices i
+JOIN clients c
+    USING (client_id)
+GROUP BY state, city  WITH ROLLUP 
