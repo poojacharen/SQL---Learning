1b6da1d941ebf38373f0ec2003ecd448e2529287
poojaraju23 <poojaraju23@gmail.com>
2025-03-06 15:07:34 -0500

    The IN Opertaor Excercise

--- WritingComplexQuery.sql
+++ WritingComplexQuery.sql
@@ -1,10 +1,10 @@
 -- The IN Operator
--- Find the products that have never been ordered
+-- Exercise : Find clients without invoices
 
+USE sql_invoicing;
 SELECT * 
-FROM products
-WHERE product_id NOT IN (
-SELECT DISTINCT product_id
-FROM order_items
+FROM clients
+WHERE client_id NOT IN (
+	 SELECT DISTINCT client_id
+     FROM invoices
 )
-
