65627c396c2b78ca38bc319888ff1d3b1adb3f31
poojaraju23 <poojaraju23@gmail.com>
2025-03-07 10:23:32 -0500

    Subqueries Vs Joins Exercise Method 1

--- WritingComplexQuery.sql
+++ WritingComplexQuery.sql
@@ -1,8 +1,17 @@
 -- Subqueries Vs Joins
--- Find clients without invoices
+-- Exercise : Find customers who have ordered lettuce (id = 3), Select customer_id, first_name, last_name
+-- Write queries using subqueries and join
+-- Method 1 : Subqueries
 
-USE sql_invoicing;
+USE sql_store;
 SELECT *
-FROM clients
-LEFT JOIN invoices USING (client_id)
-WHERE invoice_id IS NULL
+FROM customers
+WHERE customer_id IN (
+      SELECT 
+            o.customer_id
+      FROM order_items oi
+      JOIN orders o USING (order_id)
+      WHERE product_id = 3
+)
+
+
