ff7d6e14275258437bca5400b8be1fa45e4d7480
poojaraju23 <poojaraju23@gmail.com>
2025-03-07 12:30:11 -0500

    The ALL Keyword

--- WritingComplexQuery.sql
+++ WritingComplexQuery.sql
@@ -1,14 +1,15 @@
--- Subqueries Vs Joins
--- Exercise : Find customers who have ordered lettuce (id = 3), Select customer_id, first_name, last_name
--- Write queries using subqueries and join
--- Method 2 : Joins (This method is more readable)
+-- The ALL Keyword
+-- Selectinvoices larger than all invoices of client 3
 
-USE sql_store;
-SELECT DISTINCT customer_id, first_name, last_name
-FROM customers c
-JOIN orders o USING (customer_id)
-JOIN order_items oi USING (order_id)
-WHERE oi.product_id = 3
+USE sql_invoicing;
+SELECT *
+FROM invoices 
+WHERE invoice_total > ALL (
+     SELECT invoice_total
+     FROM invoices
+     WHERE client_id = 3
+)
+
 
 
 
