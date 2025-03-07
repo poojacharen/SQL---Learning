722c1fa3b1aea20ee0da8ebafa9fb29e1fd3ba07
poojaraju23 <poojaraju23@gmail.com>
2025-03-07 12:53:38 -0500

    Correlatd Subqueries Exercise

--- WritingComplexQuery.sql
+++ WritingComplexQuery.sql
@@ -1,12 +1,13 @@
 -- Correlated Subqueries
--- For each employee - calculate the avg salary for employee.office
---                     return the employee if salary > avg
+-- Exercise : Get invoices that are larger than the 
+--            client's average invoice amount
 
-USE sql_hr;
-SELECT * 
-FROM employees e
-WHERE salary > (
-     SELECT AVG(salary)
-     FROM employees
-     WHERE office_id = e.office_id
+USE sql_invoicing;
+SELECT *
+FROM invoices i
+WHERE invoice_total > (
+     SELECT AVG(invoice_total)
+     FROM invoices
+     WHERE client_id = i.client_id
 )
+
