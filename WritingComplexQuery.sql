84635c716f68fe0425f27c1055f085b13127bf7a
poojaraju23 <poojaraju23@gmail.com>
2025-03-07 13:49:59 -0500

    Subqueries in the SELECT Clause Exercise

--- WritingComplexQuery.sql
+++ WritingComplexQuery.sql
@@ -1,10 +1,17 @@
 -- Subqueries in the SELECT Clause
+-- Exercise : Write a query to produce this result which is (client_id, name, total_sales, average, difference) 
+--            total_sales of each client which is basically the sum of all invoices issued for each client. 
+--            average of all the invoice totals
+--            difference is between the total_sales and average
 
 USE sql_invoicing;
 SELECT 
-      invoice_id,
-      invoice_total,
+     client_id,
+     name,
+     (SELECT SUM(invoice_total)
+      FROM invoices
+      WHERE client_id = c.client_id) AS total_sales,
       (SELECT AVG(invoice_total)
-       FROM invoices) AS invoice_average,
-       invoice_total - (SELECT invoice_average) AS difference
-FROM invoices 
+       FROM invoices) AS average,
+       (SELECT total_sales - average) AS difference
+FROM clients c
