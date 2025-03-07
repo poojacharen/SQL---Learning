38a20aa72ee41d9498650d11bf6e72696c34310d
poojaraju23 <poojaraju23@gmail.com>
2025-03-07 12:59:44 -0500

    The EXISTS Operator

--- WritingComplexQuery.sql
+++ WritingComplexQuery.sql
@@ -1,13 +1,11 @@
--- Correlated Subqueries
--- Exercise : Get invoices that are larger than the 
---            client's average invoice amount
+-- The EXISTS Operator
+-- Select clients that have an invoice
 
 USE sql_invoicing;
 SELECT *
-FROM invoices i
-WHERE invoice_total > (
-     SELECT AVG(invoice_total)
-     FROM invoices
-     WHERE client_id = i.client_id
+FROM clients c
+WHERE EXISTS (
+     SELECT client_id
+     FROM invoices 
+     WHERE client_id = c.client_id
 )
-
