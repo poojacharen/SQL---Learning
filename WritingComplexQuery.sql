84c2469369c9a1c7e3a4dd18e3eb626c417cce56
poojaraju23 <poojaraju23@gmail.com>
2025-03-07 14:06:57 -0500

    Subqueries in the FROM Clause

--- WritingComplexQuery.sql
+++ WritingComplexQuery.sql
@@ -1,17 +1,17 @@
--- Subqueries in the SELECT Clause
--- Exercise : Write a query to produce this result which is (client_id, name, total_sales, average, difference) 
---            total_sales of each client which is basically the sum of all invoices issued for each client. 
---            average of all the invoice totals
---            difference is between the total_sales and average
+-- Subqueries in the FROM Clause
 
 USE sql_invoicing;
-SELECT 
-     client_id,
-     name,
-     (SELECT SUM(invoice_total)
-      FROM invoices
-      WHERE client_id = c.client_id) AS total_sales,
-      (SELECT AVG(invoice_total)
-       FROM invoices) AS average,
-       (SELECT total_sales - average) AS difference
+SELECT *
+FROM (
+   SELECT 
+        client_id,
+        name,
+        (SELECT SUM(invoice_total)
+         FROM invoices
+         WHERE client_id = c.client_id) AS total_sales,
+		(SELECT AVG(invoice_total)
+         FROM invoices) AS average,
+		(SELECT total_sales - average) AS difference
 FROM clients c
+) AS sales_summary
+WHERE total_sales IS NOT NULL
