d5f482888a41fb4b472589e472984092510603cc
poojaraju23 <poojaraju23@gmail.com>
2025-03-10 12:31:50 -0400

    Formatting Dates and Times

--- EssentialMySQLFunctions.sql
+++ EssentialMySQLFunctions.sql
@@ -1,18 +1,4 @@
--- Date Functions
--- NOW() We can get the current date and time
--- CURDATE() This returns the current date without the time component
--- CURTIME() This returns the current time without the date component
--- Also have some bunch of functions extracting certain components in a date or time
--- eg: YEAR(NOW()), MONTH(NOW)), DAY(NOW)), HOUR(NOW)), MINUTE(NOW)), SECOND(NOW)) : All these fun return integer values
--- Also have two useful fun that return string
--- eg: DAYNAME(NOW)), MONTHNAME(NOW))
--- EXTRACT() It is part of the standard sql language. If we want to be able to port the code to other dbms, we can use the EXTRACT fun
--- eg: EXTRACT(YEAR FROM NOW()), or MONTH< DAY, DATE etc,.. so when calling this fun we type out the unit (YEAR), then the from keyword 
---                                                       and then a date time value.
-
--- Exercise
-USE sql_store;
-SELECT *
-FROM orders
-WHERE YEAR(order_date) = YEAR(NOW())
-
+-- Formatting Dates and Times
+-- DATE_FORMAT(NOW(), '%M %D %Y') - %Y means year as a numeric value of 4-digits, %M as month in a string value, %D as date in a num of 2 digit
+-- TIME_FORMAT(NOW(), '%H:%i %p') - Returns current time: 12:58 PM - H is hour limit from 00 to 23, i is min limit from 00 to 59,
+--                                                                   p means AM or PM
