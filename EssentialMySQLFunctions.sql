572d671bcb39af502c759e65ac10b98d03a6bd91
poojaraju23 <poojaraju23@gmail.com>
2025-03-10 12:14:27 -0400

    Date Functions

--- EssentialMySQLFunctions.sql
+++ EssentialMySQLFunctions.sql
@@ -1,7 +1,11 @@
--- Numeric Functions :
--- ROUND() Returns numeric expression rounded to an integer. Can be used to round an exp. to a num of decimal points
--- TRUNCATE() Returns numeric exp1 truncated to exp2 decimal places. If exp2 is 0, then the result will have no decimal points
--- CEILING() Returns the smallest integer value that is not less than passed numeric expression
--- FLOOR() Returns the largest integer vaue that is not greater than passed numeric expression
--- ABS() Returns the absolute value of numeric expression
--- RAND() Returns a random floating-point value within the range 0 to 1.0
+-- Date Functions
+-- NOW() We can get the current date and time
+-- CURDATE() This returns the current date without the time component
+-- CURTIME() This returns the current time without the date component
+-- Also have some bunch of functions extracting certain components in a date or time
+-- eg: YEAR(NOW()), MONTH(NOW)), DAY(NOW)), HOUR(NOW)), MINUTE(NOW)), SECOND(NOW)) : All these fun return integer values
+-- Also have two useful fun that return string
+-- eg: DAYNAME(NOW)), MONTHNAME(NOW))
+-- EXTRACT() It is part of the standard sql language. If we want to be able to port the code to other dbms, we can use the EXTRACT fun
+-- eg: EXTRACT(YEAR FROM NOW()), or MONTH< DAY, DATE etc,.. so when calling this fun we type out the unit (YEAR), then the from keyword 
+--                                                          and then a date time value.
