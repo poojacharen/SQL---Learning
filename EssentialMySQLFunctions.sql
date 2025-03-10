0db401a1b2615f94210ad509d062b721edff5d81
poojaraju23 <poojaraju23@gmail.com>
2025-03-10 14:55:48 -0400

    The CASE Operator Exercise

--- EssentialMySQLFunctions.sql
+++ EssentialMySQLFunctions.sql
@@ -1,13 +1,15 @@
 -- The CASE Operator
--- It's used when we have multiple expressions to test
+-- Exercise : if a customer has more than 3000 points classify them as Gold customers, if they have bet 2000 and 3000 classify them as Silver
+--          customers, otherwise of they have less than 2000 classify them as Bronze customers. Three cols (customer, points, category)
 
 USE sql_store;
 SELECT 
-     order_id,
+     CONCAT(first_name, ' ', last_name) AS customer,
+     points,
      CASE 
-         WHEN YEAR(order_date) = YEAR(NOW()) THEN "Active"
-         WHEN YEAR(order_date) = YEAR(NOW()) -1 THEN "Last Year"
-         WHEN YEAR(order_date) < YEAR(NOW()) -1 THEN "Archived"
-         ELSE "Future"
-	END AS category
-FROM orders
+         WHEN points > 3000 THEN "Gold"
+         WHEN points BETWEEN 2000 AND 3000 THEN "Silver"
+         WHEN points < 2000 THEN "Bronze"
+	 END AS category
+FROM customers
+     
