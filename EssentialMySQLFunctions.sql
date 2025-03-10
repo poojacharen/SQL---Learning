a27adf20968e0a6426d4f99b43b23e55342f5d42
poojaraju23 <poojaraju23@gmail.com>
2025-03-10 14:35:11 -0400

    The IF Function Exercise

--- EssentialMySQLFunctions.sql
+++ EssentialMySQLFunctions.sql
@@ -1,9 +1,14 @@
 -- The IF Function
--- Return the order date of this year as Active, for the past year as Archived
+-- Exercise : Write a query to produce these columns product_id, name, orders, frequency, frequency column which is based on what we have 
+--            in the orders column. If a product has been ordered more than once in a frequency column we have Many times, otherwise Once
 
 USE sql_store;
 SELECT 
-      order_id,
-      order_date,
-      IF(YEAR(order_date) = YEAR(NOW()), "Active", "Archived") AS category
-FROM orders
+      product_id,
+      name,
+      COUNT(*) AS orders,
+      IF(COUNT(*) > 1, "Many times", "Once") AS frequency
+FROM products
+JOIN order_items USING (product_id)
+GROUP BY product_id, name
+      
