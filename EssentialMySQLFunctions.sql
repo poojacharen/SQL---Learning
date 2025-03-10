10b3789fa6d6cd81363230ea0b8c9bea860aca33
poojaraju23 <poojaraju23@gmail.com>
2025-03-10 14:28:15 -0400

    The IF Function

--- EssentialMySQLFunctions.sql
+++ EssentialMySQLFunctions.sql
@@ -1,13 +1,9 @@
--- The IFNULL and COALESCE Functions
--- With IFNULL func - we can substitute a null value, with something else
---                    eg: IFNULL(shipper_id, 'Not assigned')
--- With COALESCE func - we supply a list of values, and this func will return the first non null value in the list 
---                    eg: COALESCE(shipper_id, comments, 'Not assigned') 
-
--- Exercise : We have two columns customer and phone. In the customer column, we display the customers full name and in the phone column we 
---            display their phone num, and if we don't have their phone num, we display Unknown
+-- The IF Function
+-- Return the order date of this year as Active, for the past year as Archived
 
 USE sql_store;
-SELECT  CONCAT(first_name, ' ', last_name) AS customer,
-		IFNULL(phone, "Unknown") AS phone
-FROM customers
+SELECT 
+      order_id,
+      order_date,
+      IF(YEAR(order_date) = YEAR(NOW()), "Active", "Archived") AS category
+FROM orders
