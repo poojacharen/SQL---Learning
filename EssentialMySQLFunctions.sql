678c56a326c2da345007b0a71ca44ca7e54b80d2
poojaraju23 <poojaraju23@gmail.com>
2025-03-10 14:21:41 -0400

    The IFNULL and COALESCE Functions meaning and exercise

--- EssentialMySQLFunctions.sql
+++ EssentialMySQLFunctions.sql
@@ -1,12 +1,13 @@
--- Calculating Date and Times                                                              p means AM or PM
--- DATE_ADD - This function used to add a date 
--- DATE_ADD(NOW(), INTERVAL 1 DAY) - Returns tomorrow's date with the same time
--- DATE_ADD(NOW(), INTERVAL 1 YEAR) - Returns next year, same day, same hr
--- DATE_ADD(NOW(), INTERVAL -1 YEAR)- What if we want to go back in time, we either pass negative value like -1, so this returns last yr's date
--- DATE_SUB(NOW(), INTERVAL 1 YEAR) - Instead of -1, we can give SUB so it subtracts a date part from a date time value
--- We can also calculate the difference between 2 dates
--- DATEDIFF("2019-01-05", "2019-01-01") - This func only returns the difference in days, not the hrs or mins even if we include a time value
--- We can also calculate the difference between 2 times
--- TIME_TO_SEC() - This returns the num of sec elapsed since midnight
---                 eg: TIME_TO_SEC("09:00") - It returns the result as 32400, these are the num of sec elapsed since midnight
--- TIME_TO_SEC("09:00") - TIME_TO_SEC("09:02") - We get -120 cause we have set the time earlier when we swap the values its 120
+-- The IFNULL and COALESCE Functions
+-- With IFNULL func - we can substitute a null value, with something else
+--                    eg: IFNULL(shipper_id, 'Not assigned')
+-- With COALESCE func - we supply a list of values, and this func will return the first non null value in the list 
+--                    eg: COALESCE(shipper_id, comments, 'Not assigned') 
+
+-- Exercise : We have two columns customer and phone. In the customer column, we display the customers full name and in the phone column we 
+--            display their phone num, and if we don't have their phone num, we display Unknown
+
+USE sql_store;
+SELECT  CONCAT(first_name, ' ', last_name) AS customer,
+		IFNULL(phone, "Unknown") AS phone
+FROM customers
