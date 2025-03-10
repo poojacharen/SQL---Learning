56e9981bc3823a5e5692071e54539b0a9f33e8f7
poojaraju23 <poojaraju23@gmail.com>
2025-03-10 11:42:45 -0400

    Numeric Functions

---
+++ EssentialMySQLFunctions.sql
@@ -0,0 +1,7 @@
+-- Numeric Functions :
+-- ROUND() Returns numeric expression rounded to an integer. Can be used to round an exp. to a num of decimal points
+-- TRUNCATE() Returns numeric exp1 truncated to exp2 decimal places. If exp2 is 0, then the result will have no decimal points
+-- CEILING() Returns the smallest integer value that is not less than passed numeric expression
+-- FLOOR() Returns the largest integer vaue that is not greater than passed numeric expression
+-- ABS() Returns the absolute value of numeric expression
+-- RAND() Returns a random floating-point value within the range 0 to 1.0
