1da872d7d0e74c8f965c47c927bf5dc15b03cf70
poojaraju23 <poojaraju23@gmail.com>
2025-03-10 16:40:36 -0400

    Updated InsertingUpdatingAndDeletingData.sql and removed unused files

--- InsertingUpdatingAndDeletingData.sql
+++ InsertingUpdatingAndDeletingData.sql
@@ -1,4 +1,2 @@
--- Restoring the Databases 
--- go to the file menu --> open sql script --> navigate to the queries where we stored --> open create-databases.sql --> execute the scrip to 
---  recreate all of our databases
+
 
