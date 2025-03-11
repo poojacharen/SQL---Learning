01b8def90999805ffeff98a2db9429730197a9cf
poojaraju23 <poojaraju23@gmail.com>
2025-03-11 12:03:45 -0400

    Updatable Views

--- Views.sql
+++ Views.sql
@@ -1,16 +1,25 @@
--- Altering or Dropping Views - Once we create a View, we may realize the query has a problem, so we may need to go back and change the view.
--- Two ways : 1. Drop the view and then recreate it - DROP VIEW sales_by_client  after executing this the view will be gone. 
---            2. Recreate the view - Again we can go back to the query and recreate it  and other way is to use the REPLACE keyword.
---               We can change the CREATE VIEW statement like this - CREATE OR REPLACE VIEW ........ AS 
+-- Updatable Views - We have only used views for SELECT statements so far. BUt we can also use them in insert, update and delete statements
+--                   but only under certain circumstances
+-- If the view doesn't have the DISTINCT keyword and any Aggregate Functions like MIN, MAX, SUM etc..., GROUP BY / HAVING 
+--                                                       and these basically go together because  most of the time when we use an aggregate
+--                                                       func we also have a GROUP BY clause as well.
+-- Finally, the UNION operator.  If we don't have any of these above stuff in our View, then we refer to that view as an Updatable Views which means
+-- we can update data through it.  So, we can use that view in insert, update and delete statements.
+
+-- We are going to create a balance for invoice table so that we don't need to create it everytime
 
 USE sql_invoicing;
-CREATE VIEW clients_balances AS
+CREATE OR REPLACE VIEW invoices_balance AS
 SELECT 
-      c.client_id,
-      c.name,
-      SUM(invoice_total - payment_total) AS balance
-FROM clients c
-JOIN invoices i 
-    USING (client_id)
-GROUP BY client_id, name
-
+     invoice_id,
+     number,
+     client_id,
+     invoice_total,
+     payment_total,
+     invoice_total - payment_total AS balance,
+     invoice_date,
+     due_date,
+     payment_date
+FROM invoices
+WHERE (invoice_total - payment_total) > 0
+     
