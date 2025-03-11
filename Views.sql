97c25213770f893ce9972fda38198e6e22518b84
poojaraju23 <poojaraju23@gmail.com>
2025-03-11 11:45:59 -0400

    Altering and Dropping Views

--- Views.sql
+++ Views.sql
@@ -1,8 +1,7 @@
--- Creating Views : View is extremely powerful and it can greatly simplify our future queries. View behaves like a virtual table, but views don't
---                  store data, our data is actually stored in our tables. View just provides a view to the underlying tables ie. why its 
---                  called Views
--- Exercise : Create a view to see the balance for each client. clients_balance
---            client_id, name, balance - we calculate balance by subtracting the payment_total from invoice_total in invoices table
+-- Altering or Dropping Views - Once we create a View, we may realize the query has a problem, so we may need to go back and change the view.
+-- Two ways : 1. Drop the view and then recreate it - DROP VIEW sales_by_client  after executing this the view will be gone. 
+--            2. Recreate the view - Again we can go back to the query and recreate it  and other way is to use the REPLACE keyword.
+--               We can change the CREATE VIEW statement like this - CREATE OR REPLACE VIEW ........ AS 
 
 USE sql_invoicing;
 CREATE VIEW clients_balances AS
