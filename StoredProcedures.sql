664548abd658b6d49b47c195f6b5ab00b2f88453
poojaraju23 <poojaraju23@gmail.com>
2025-03-13 12:23:04 -0400

    Creating a SP Exercise

--- StoredProcedures.sql
+++ StoredProcedures.sql
@@ -1,9 +1,13 @@
 -- Creating a Stored Procedure
+-- Exercise : Create a SP called get_invoices_with_balance, to return all the invoices with a balance > 0
+-- I took the FROM statement from Views
 
 DELIMITER $$
-CREATE PROCEDURE get_clients()
-SELECT * 
-FROM clients;
+CREATE PROCEDURE get_invoices_with_balance()
+BEGIN
+SELECT *
+FROM invoices_balances
+WHERE balance > 0;
 END$$
 
 DELIMITER ;
