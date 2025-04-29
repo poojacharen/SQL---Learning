30dc9a07473fba824d9b2fe402f77ee907377fdd
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-04-29 12:50:44 -0400

    Triggers - Viewing Triggers

---
+++ Triggers - Viewing Triggers.sql
@@ -0,0 +1,6 @@
+-- Viewing Triggers - using SHOW TRIGGERS we can view all the triggers from the current database
+
+SHOW TRIGGERS LIKE 'payments%'   -- We can use the conventions to see the triggers
+
+-- we add the tablename, before or after and finally the type of the SQL statement which can be insert, update or delete
+--    tablename_after_insert
