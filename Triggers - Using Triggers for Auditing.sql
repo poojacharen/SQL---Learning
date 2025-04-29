25684b5e0a85f609117139b4a501bb66451d13b1
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-04-29 13:18:55 -0400

    Triggers - Using Triggers for Auditing

--- Triggers - Using Triggers for Auditing.sql
+++ Triggers - Using Triggers for Auditing.sql
@@ -1,1 +1,43 @@
+-- Using Triggers for Auditing
 
+-- after insert
+DELIMITER $$
+
+DROP TRIGGER IF EXISTS payments_after_insert;
+
+CREATE TRIGGER payments_after_insert
+	AFTER DELETE ON payments
+    FOR EACH ROW 
+BEGIN
+	UPDATE invoices 
+    SET payment_total = payment_total + NEW.amount
+    WHERE invoice_id = NEW.invoice_id;
+    
+    INSERT INTO payments_audit
+    VALUES (NEW.client_id, NEW.date, NEW.amount, 'Insert', NOW());
+END $$
+
+DELIMITER ;
+
+-- after deletion
+DELIMITER $$
+
+DROP TRIGGER IF EXISTS payments_after_delete;
+
+CREATE TRIGGER payments_after_delete
+	AFTER DELETE ON payments
+    FOR EACH ROW 
+BEGIN
+	UPDATE invoices 
+    SET payment_total = payment_total - OLD.amount
+    WHERE invoice_id = OLD.invoice_id;
+    
+    INSERT INTO payments_audit
+    VALUES (OLD.client_id, OLD.date, OLD.amount, 'Delete', NOW());
+END $$
+
+DELIMITER ;
+
+-- In a new query, after tyoing the below query it creates record on the new audits table
+INSERT INTO payments
+VALUES (DEFAULT, 5, 3, '2019-01-01', 10, 1)
