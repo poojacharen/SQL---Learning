62cb014eabd62948e4bb292d3b473cf237450e71
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-05-12 13:26:25 -0400

    Securing Databases - Viewing Users

---
+++ Securing Databases - Viewing Users.sql
@@ -0,0 +1,7 @@
+-- Viewing Users
+-- Let's see the list of users in our MySQL server, there are basically 2 ways to do
+-- We have a datbase called Mysql and we have a table called user so we can select everything from this table 
+
+SELECT * FROM mysql.user;
+-- Above is the one way to view the users
+-- Second way is to view through the navigator panel on the left side, Open the administration tab --> under management --> user and priveleges
