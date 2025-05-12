472d906cbedd9c22c3dcc9e8ee7edbcf4672d4f1
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-05-12 14:00:08 -0400

    Securing Databases - Viewing Privileges

---
+++ Securing Databases - Viewing Privileges.sql
@@ -0,0 +1,10 @@
+-- Viewing Privileges 
+-- There are times that we want to see the permissions or privileges assigned to a user account
+-- There are basically 2 ways to do this 
+-- 1. we can do it through navigator panel or
+-- 2. using sql
+
+SHOW GRANTS FOR pooja;
+
+-- 1. administration tab --> management --> user and privileges --> select pooja --> under administrative roles --> we can see the roles and 
+--    privileges assigned to this user  --> under schema privileges we can see the specific permissions for various databases 
