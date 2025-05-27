2700cf431f9d49561dc18522a67d5fee8d196e40
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-05-27 14:29:13 -0400

    Designing Databases - Storage Engines

---
+++ Designing Databases - Storage Engines.sql
@@ -0,0 +1,7 @@
+-- Storage Engines
+-- In MySQL we have several storage engines and in these storage engines determine how the data is stored and what features are available to us.
+-- The 2 most commonly used storage engines are MyISAM and InnoDB
+-- MyISAM is an older storage engine, it is used to be popular prior to version 5.5
+-- But going forward InnoDB is the superior storage engine, it supports features like transactions, etc... MyISAM doesn't support these features
+
+SHOW ENGINES 
